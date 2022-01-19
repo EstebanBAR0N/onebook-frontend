0df029055345619864045bf69a0e1bb078aa9fbb
esteban <esteban.baron@etu.umontpellier.fr>
2022-01-19 19:43:42 +0100

    Change title color

--- src/components/OneBookButton.js
+++ src/components/OneBookButton.js
@@ -1,11 +1,17 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@mui/styles';
+import { useTheme } from "@material-ui/styles";
+
 
 import '../assets/styles/fonts.css';
     
+
 function OneBookButton() {
+  const theme = useTheme();
+
   return (
     // web site title
     <Box container>
@@ -13,7 +19,8 @@ function OneBookButton() {
         fontSize: { xs: "20px", sm: "24px", md: "30px" }, 
         marginRight: { xs: "1em" },
       }}>
-        <Link to="/" style={{
+        <Link to="/"   style={{
+          color: theme.palette.primary.main,
           fontFamily: 'Hammersmith One', 
           textDecoration: "none", 
         }}
