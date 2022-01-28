6500b719680433067e0ca96c2eb6bb05f9abbf51
esteban <esteban.baron@etu.umontpellier.fr>
2022-01-28 10:48:52 +0100

    Add file name in audio file component view

--- src/components/user/UserContent.js
+++ src/components/user/UserContent.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import Box from '@mui/material/Box';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import Typography from '@mui/material/Typography';
 import { useTheme } from "@material-ui/core/styles";
 // import { makeStyles } from '@mui/styles';
 
@@ -40,37 +42,71 @@ function UserContent(props) {
         gap={100}
       >
         {/* image container */}
-        {props.files && props.files.map((file) => (
-          <ImageListItem 
-            key={file.id} 
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              width: '17em',
-              height: '17em',
-              border: '1px solid',
-              borderColor: theme.palette.LIGHT_GREY.main,
-              borderRadius: (file.format === 'audio' ? '30px' : 0),
-              backgroundPosition: 'center',
-              backgroundSize: 'cover',
-            }}
-          >
-            {/* media  */}
-            <CardMedia
-              onClick={file.format === 'image' ? props.onClick : null}
-              component={file.format === 'image' ? 'img' : file.format === 'video' ? 'video' : 'audio'} 
-              controls
-              image={file.url}
-              alt={file.format}
-              loading='lazy'
+        {props.files && props.files.map((file) => {
+
+          // get name of file by url
+          const url = file.url.split('/');
+          let file_name = url[url.length - 1].split('.').slice(0, -1).join(' ');
+
+          return (
+            <ImageListItem 
+              key={file.id} 
               sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                width: '17em',
+                height: '17em',
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+              }}
+            >
+              {/* media  */}
+              <Card sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                borderRadius: (file.format === 'audio' ? '30px' : 0),
+                marginBottom: '10px',
                 width: '17em',
                 height: '17em',
                 objectFit: 'cover',
-              }}
-            />
-          </ImageListItem>
-        ))}
+              }}>
+                <Box sx={{
+                  display:(file.format === 'audio' ? 'flex' : 'none'),
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  height: '100%',
+                }}>
+                  <Typography
+                    style={{ wordWrap: "break-word" }}
+                    gutterBottom
+                    variant="h5"
+                    component="h5"
+                    sx={{ 
+                      marginTop: '1em', 
+                      width: '100%',
+                      textAlign: 'center',
+                    }}
+                  >
+                    {file_name}
+                  </Typography>
+                </Box>
+                <CardMedia
+                  onClick={file.format === 'image' ? props.onClick : null}
+                  component={file.format === 'image' ? 'img' : file.format === 'video' ? 'video' : 'audio'} 
+                  controls
+                  image={file.url}
+                  alt={file.format}
+                  loading='lazy'
+                  sx={{
+                    width: '17em',
+                    height: (file.format === 'audio' ? '4em' : '17em'),
+                    objectFit: 'cover',
+                  }}
+                />
+              </Card>
+            </ImageListItem>
+          )
+        })}
       </ImageList>
     </Box>
   );
